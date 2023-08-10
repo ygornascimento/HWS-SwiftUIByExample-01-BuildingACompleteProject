d846bea5a75d7e21b4d6684aefaea0b523c53c8d
Ygor Nascimento <ygor.nascimento@outlook.com>
2023-08-10 18:18:45 -0300

    Presenting an alert

--- HWSSwiftUIByExample/HWSSwiftUIByExample/CheckoutView.swift
+++ HWSSwiftUIByExample/HWSSwiftUIByExample/CheckoutView.swift
@@ -13,6 +13,7 @@ struct CheckoutView: View {
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
     @State private var tipAmount = 15
+    @State private var showingPaymentAlert = false
     
     let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
     let tipAmounts = [10, 15, 20,25,0]
@@ -45,12 +46,17 @@ struct CheckoutView: View {
             }
             Section("Total: \(totalPrice)") {
                 Button("Confirm order") {
-                    
+                    showingPaymentAlert.toggle()
                 }
             }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
+        .alert("Order Confirmed", isPresented: $showingPaymentAlert) {
+            
+        } message: {
+            Text("Your total was \(totalPrice) - thank you!")
+        }
     }
 }
 
